64f3e6a68da28588d33ee992210ecd70907b76f3
Nicholas Manning <njmanning212@gmail.com>
2023-04-11 13:38:46 -0500

    upload rookie digimon info into data and export

---
+++ data/digimon.js
@@ -0,0 +1,13 @@
+rookieDigimon = [
+  {'name': 'Agumon', '_id': '002', 'partner': 'Tai', 'main_attack': 'Pepper Breath'},
+  {'name': 'Gabumon', '_id': '003', 'partner': 'Matt', 'main_attack': 'Blue Blaster'},
+  {'name': 'Biyomon', '_id': '004', 'partner': 'Sora', 'main_attack': 'Spiral Twister'},
+  {'name': 'Tentomon', '_id': '005', 'partner': 'Izzy', 'main_attack': 'Super Shocker'},
+  {'name': 'Palmon', '_id': '006', 'partner': 'Mimi', 'main_attack': 'Poison Ivy'},
+  {'name': 'Gomamon', '_id': '007', 'partner': 'Joe', 'main_attack': 'Marching Fishes'},
+  {'name': 'Patamon', '_id': '008', 'partner': 'T.K.', 'main_attack': 'Boom Bubble'}
+]
+
+export {
+  rookieDigimon
+}
